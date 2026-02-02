d52820acb1e5839697762b66a1fe99320ceef742
vishvaa <varikrishnan17@gmail.com>
2026-02-02 21:13:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
-const noBtn = document.getElementById("no");
-const yesBtn = document.getElementById("yes");
-const message = document.getElementById("message");
+window.onload = function () {
+  const noBtn = document.getElementById("no");
+  const yesBtn = document.getElementById("yes");
+  const message = document.getElementById("message");
 
-noBtn.addEventListener("mouseover", () => {
-  const x = Math.random() * 200 - 100;
-  const y = Math.random() * 200 - 100;
-  noBtn.style.transform = translate(${x}px, ${y}px);
-});
+  if (!noBtn || !yesBtn || !message) {
+    console.error("One or more elements not found");
+    return;
+  }
 
-yesBtn.addEventListener("click", () => {
-  message.innerHTML = "Yayyy! 💘 I knew you'd say yes!";
-});
+  noBtn.addEventListener("mouseover", function () {
+    const x = Math.random() * 300 - 150;
+    const y = Math.random() * 300 - 150;
+    noBtn.style.transform = "translate(" + x + "px, " + y + "px)";
+  });
+
+  yesBtn.addEventListener("click", function () {
+    message.innerHTML = "Yayyy! 💘 I knew you'd say yes!";
+  });
+};
